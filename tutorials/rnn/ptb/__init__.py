5928bce64fb659498cf1157b92d1c3242270870a
PeterDing <dfhayst@gmail.com>
2018-06-23 12:58:33 +0800

    [tutorials/rnn/ptb] [PEP8]

--- tutorials/rnn/ptb/__init__.py
+++ tutorials/rnn/ptb/__init__.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
 """Makes helper libraries available in the ptb package."""
 from __future__ import absolute_import
 from __future__ import division
